1c2202e08a319da88b33892ac4cc8a4dea352b9a
Richard McSharry <rmcsharry@gmail.com>
2020-04-17 22:06:58 +0200

    Adds enrollment serializer

--- cbmanager/chatterbox/serializers.py
+++ cbmanager/chatterbox/serializers.py
@@ -1,9 +1,8 @@
 from rest_framework import serializers
-from .models import Organisation, Course, Student
+from .models import Organisation, Course, Student, Enrollment
 import datetime
 from django.utils.timezone import make_aware
 
-# Organisation Serializer
 class OrganisationSerializer(serializers.ModelSerializer):
   students = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='student-detail')
 
@@ -11,16 +10,36 @@ class OrganisationSerializer(serializers.ModelSerializer):
     model = Organisation
     fields = ('id', 'name', 'students')
 
-# Course Serializer
 class CourseSerializer(serializers.ModelSerializer):
   class Meta:
     model = Course
     fields = '__all__'
 
+class EnrollmentSerializer(serializers.ModelSerializer):
+  student = serializers.HyperlinkedRelatedField(many=False, read_only=True, view_name='student-detail')
+  course = serializers.HyperlinkedRelatedField(many=False, read_only=True, view_name='course-detail')
+  end_date = serializers.SerializerMethodField()
+  is_active = serializers.SerializerMethodField()
+  is_current = serializers.SerializerMethodField()
 
-    # Student Serializer
+  class Meta:
+    model = Enrollment
+    fields = ('id', 'student', 'course', 'enrolled', 'end_date', 'is_current', 'last_booking', 'is_active', 'credits_balance')
+
+  # allow 1 extra day beyond 26 weeks for calculating when an enrollment ends
+  # (because midnight is actually the start of a new day, not the end)
+  def get_end_date(self, obj):
+    return obj.end_date
+
+  def get_is_active(self, obj):
+    return obj.is_active
+
+  def get_is_current(self, obj):
+    return obj.is_current
+
 class StudentSerializer(serializers.ModelSerializer):
+  enrollments = EnrollmentSerializer(many=True, read_only=True)
   organisation = serializers.HyperlinkedRelatedField(many=False, read_only=True, view_name='organisation-detail')
   class Meta:
     model = Student
-    fields = ('id', 'first_name', 'last_name', 'email', 'organisation')
+    fields = ('id', 'first_name', 'last_name', 'email', 'organisation', 'enrollments')
