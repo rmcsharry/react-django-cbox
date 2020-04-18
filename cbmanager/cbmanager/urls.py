9e3e87af11638d761f3c74d0d3c225d5c1a88452
Richard McSharry <rmcsharry@gmail.com>
2020-04-18 17:16:30 +0200

    Setup React, add first component

--- cbmanager/cbmanager/urls.py
+++ cbmanager/cbmanager/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 
 urlpatterns = [
+    path('', include('frontend.urls')),
     path('', include('chatterbox.urls')),
     path('admin/', admin.site.urls),
 ]
